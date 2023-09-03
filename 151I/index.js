025c92a5e5db4fe149b5dcf1d243afcd1de660d8
chinzcode <chinzcoding@gmail.com>
2023-09-03 20:58:57 +0200

    made changes to 151I

--- 151I/index.js
+++ 151I/index.js
@@ -1,15 +1,3 @@
-/*  14
-1   #            #
-2    ##        ##
-3     ###    ###
-4      ########
-5      ########
-6     ###    ###
-7    ##        ##
-8   #            #  
-*/
-
-
 function writeHash() {
     document.getElementById('output').innerText += '#';
 }
@@ -22,11 +10,16 @@ function writeLineBreak() {
     document.getElementById('output').innerText += '\n';
 }
 
-function writeTopHalf() {
+updateView();
+function updateView() {
+    writeTopHalf();
+    writeBottomHalf();
+}
 
+function writeTopHalf() {
     for (let row = 0; row < 4; row++) {
-        let spaceAfter = 12 - 4 * row;
         let spaceBefore = 0 + 1 * row; 
+        let spaceAfter = 12 - 4 * row;
         let hash = 1 + 1 * row;
 
         for (let printSpace = 0; printSpace < spaceBefore; printSpace++) {
@@ -48,55 +41,38 @@ function writeTopHalf() {
     }
 }
 
-writeTopHalf();
+function writeBottomHalf() {
+    for (let row = 0; row < 4; row++) {
+        let spaceBefore = 3 - 1 * row;
+        let spaceAfter = 0 + 4 * row;
+        let hash = 4 - 1 * row;
 
+    for (let printSpace = 0; printSpace < spaceBefore; printSpace++) {
+            writeSpace();
+        }
 
+        for (let i = 0; i < hash; i++) {
+            writeHash();
+        }
+        
+        for (let printSpace = 0; printSpace < spaceAfter; printSpace++) {
+            writeSpace();
+        }
+        
+        for (let i = 0; i < hash; i++) {
+            writeHash();
+        }
+        writeLineBreak();
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let test2 = 0
-// for (let row = 0; row < 4; row++) {
-//     test2++;
-//     for (let printSpace = 4; printSpace > test2; printSpace--) {
-//         writeSpace();
-//     }
-//     writeHash();
-//     writeLineBreak();
-// }
-
-// function writeTopHalf() {
-//     let test = 4
-//     for (let row = 0; row < 4; row++) {
-//         test--;
-//         for (let printSpace = 3; printSpace > test; printSpace--) {
-//             writeSpace();
-//         }
-//         writeHash();
-//         writeLineBreak();
-//     }
-//     let test2 = 0
-//     for (let row = 0; row < 4; row++) {
-//         test2++;
-//         for (let printSpace = 4; printSpace > test2; printSpace--) {
-//             writeSpace();
-//         }
-//         writeHash();
-//         writeLineBreak();
-//     }
-// }
+/*  14
+1   #            #
+2    ##        ##
+3     ###    ###
+4      ########
+5      ########
+6     ###    ###
+7    ##        ##
+8   #            #  
+*/
