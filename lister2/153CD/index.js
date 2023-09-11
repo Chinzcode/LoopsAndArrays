036b9926ff66bf1b9d68da9d9363155e4a77c154
chinzcode <chinzcoding@gmail.com>
2023-09-11 22:09:28 +0200

    added task 153D

--- lister2/153CD/index.js
+++ lister2/153CD/index.js
@@ -6,10 +6,11 @@
 3. Finne innholdet på samme index i secretCode
 4. printe ut innholdet henta fra secretCode
 */
-
+//Oppgave 153C
 let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','O','P','Q','R','S','T','U','V','W','X','Y','Z','Æ','Ø','Å'];
 let secretCode = ['Å','Ø','Æ','Y','U','O','I','E','A','B','C','D','F','G','H','J','K','L','M','P','Q','R','S','T','V','W','X','Z'];
 
+encryptText('TERJE');
 function encryptText(text) {
     let secretText = '';
     for (let i = 0; i < text.length; i++) {
@@ -18,6 +19,20 @@ function encryptText(text) {
         let secretChar = secretCode[returnElementIndex]; // secretCode[18] = M. T in normal alphabet = M in secretCode.
         secretText += secretChar;
     }
-    return secretText;
+    console.log('TERJE =', secretText);
 }
-console.log(encryptText('TERJE'));
+
+//Oppgave 153D
+//Lag en funksjon som dekrypterer en kodet tekst.
+
+decryptText('MUKBU');
+function decryptText(text) {
+    let decryptedText = '';
+    for (let i = 0; i < text.length; i++) {
+        let x = text.charAt(i); 
+        let y = secretCode.indexOf(x); 
+        let z = alphabet[y]; 
+        decryptedText += z;
+    }
+    console.log('MUKBU =', decryptedText);
+}
